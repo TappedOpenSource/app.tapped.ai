a178b56a2137d16ea16f43fb1bad1a004a6739f4
Johannes Naylor <jonaylor89@gmail.com>
2023-07-22 12:56:21 +0200

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
