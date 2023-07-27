49408feae16f0956182aae629145f3fe6ccbc029
Armaan Hossain <armaanhossain213@gmail.com>
2023-07-27 16:51:22 -0400

    added multi-select question and dropdown select question format as models to be used for future question format

--- pages/new_generator.tsx
+++ pages/new_generator.tsx
@@ -5,13 +5,14 @@ import { submitCreateGeneratorForm } from "../domain/usecases/create_generator";
 const NewGenerator = () => {
   const [name, setName] = useState("");
   const [artistName, setArtistName] = useState("");
-  const [genres, setGenres] = useState("");
+  const [genres, setGenres] = useState([]);
   const [socialFollowing, setSocialFollowing] = useState("");
   const [postFreq, setPostFreq] = useState("");
   const [sellingPoint, setSellingPoint] = useState("");
   const [theme, setTheme] = useState("");
   const [planLength, setPlanLength] = useState("");
   const [referenceImages, setReferenceImages] = useState([]);
+  const options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"];
 
   const handleFileChange = (e) => {
     setReferenceImages([...referenceImages, ...Array.from(e.target.files)]);
@@ -20,6 +21,14 @@ const NewGenerator = () => {
   const handleRemoveImage = (indexToRemove) => {
     setReferenceImages(
       referenceImages.filter((_, index) => index !== indexToRemove)
+    );
+  };
+
+  const handleSelectChange = (option) => {
+    setGenres((prevOptions) =>
+      prevOptions.includes(option)
+        ? prevOptions.filter((prevOption) => prevOption !== option)
+        : [...prevOptions, option]
     );
   };
 
@@ -51,7 +60,7 @@ const NewGenerator = () => {
             <div className="md:w-1/3">
               <label
                 className="mb-1 block pr-4 text-xs font-bold text-gray-500 md:mb-0 md:text-right"
-                htmlFor="inline-artistName"
+                htmlFor="inline-name"
               >
                 What do you want to call this model?
               </label>
@@ -59,7 +68,7 @@ const NewGenerator = () => {
             <div className="md:w-2/3">
               <input
                 className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-                id="inline-artistName"
+                id="inline-name"
                 type="text"
                 placeholder=""
                 onChange={(e: any) => setName(e.target.value)}
@@ -92,25 +101,26 @@ const NewGenerator = () => {
           </div>
 
           <div className="mb-6 md:flex md:items-center">
-            <div className="md:w-1/3">
-              <label
-                className="mb-1 block pr-4 text-xs font-bold text-gray-500 md:mb-0 md:text-right"
-                htmlFor="inline-artistName"
-              >
-                What 3 genres would you use to describe your music?
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
-                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-                id="inline-genres"
-                type="text"
-                placeholder=""
-                onChange={(e: any) => setGenres(e.target.value)}
-                value={genres}
-                required
-              ></input>
-            </div>
+            <label className="mb-1 block pr-4 text-xs font-bold text-gray-500 md:mb-0 md:text-right">
+              What genres would you use to describe your music?
+              <div className="grid grid-cols-2 gap-2 pt-4">
+                {options.map((option, index) => (
+                  <div key={index} className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      id={`option${index}`}
+                      value={option}
+                      checked={genres.includes(option)}
+                      onChange={() => handleSelectChange(option)}
+                      className="form-checkbox h-5 w-5 text-indigo-600 focus:outline-none"
+                    />
+                    <label className="text-sm" htmlFor={`option${index}`}>
+                      {option}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </label>
           </div>
 
           <div className="mb-6 md:flex md:items-center">
@@ -162,21 +172,24 @@ const NewGenerator = () => {
             <div className="md:w-1/3">
               <label
                 className="mb-1 block pr-4 text-xs font-bold text-gray-500 md:mb-0 md:text-right"
-                htmlFor="inline-theme"
+                htmlFor="dropdown-input"
               >
                 What should the theme for your image content be?
               </label>
             </div>
             <div className="md:w-2/3">
-              <input
-                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-                id="inline-theme"
-                type="text"
-                placeholder=""
+              <select
+                id="dropdown-theme"
                 onChange={(e: any) => setTheme(e.target.value)}
-                value={theme}
-                required
-              ></input>
+                className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+              >
+                <option value=""> </option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+                <option value="option4">Option 4</option>
+                <option value="option5">Option 5</option>
+              </select>
             </div>
           </div>
 
