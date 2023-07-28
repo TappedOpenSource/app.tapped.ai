51c2547b307499c2b965ce31b291a4beacaf59b9
Johannes Naylor <jonaylor89@gmail.com>
2023-07-28 18:08:16 +0200

    push

--- gcloud_functions/src/utils/leapai.ts
+++ gcloud_functions/src/utils/leapai.ts
@@ -5,11 +5,38 @@ import * as logger from 'firebase-functions/logger';
 // const STAGE_PHOTOS_PROMPT = '';
 // const ALBUM_ART_PROMPT = '';
 
-export const callSdModel = async ({
+export const createInferenceJob = async ({
+  modelId,
   prompt,
 }: {
+    modelId: string;
     prompt: string;
 }): Promise<{ text: string }> => {
   logger.log(`Generating text ${prompt}`);
+
+  const { data, error } = await leap.generate.createInferenceJob({
+    prompt: 'A photo of a cat',
+  });
+
   return { text: '' };
+};
+
+export const getInferenceJob = async ({
+  inferenceId,
+}: {
+        inferenceId: string;
+}) => {
+  const { data, error } = await leap.generate.getInferenceJob({
+    inferenceId: 'inferenceId',
+  });
+};
+
+export const deleteInferenceJob = async ({
+  inferenceId,
+}: {
+    interenceId: string;
+}) => {
+  const { data, error } = await leap.generate.deleteInference({
+    inferenceId: 'inferenceId',
+  });
 };
