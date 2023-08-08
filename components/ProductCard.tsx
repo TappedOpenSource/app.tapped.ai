b5a0ac4b0a87a8242000ccee39b260f43eb6b3d1
Armaan Hossain <armaanhossain213@gmail.com>
2023-08-08 17:21:48 -0400

    loading icon

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,6 +1,6 @@
+import { useState } from 'react';
+import { Button, CircularProgress } from '@mui/material';
 import { subscribe } from '@/domain/usecases/payments';
-import { Button } from '@mui/material';
-import Image from 'next/image';
 
 const ProductCard = ({ product, prices }: { product: any; prices: any[] }) => {
   const priceData = prices[0];
@@ -10,6 +10,18 @@ const ProductCard = ({ product, prices }: { product: any; prices: any[] }) => {
   }).format(((priceData.unit_amount as number) / 100).toFixed(2))}/${
     priceData.interval ?? 'once'
   }`;
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubscribe = async () => {
+    setLoading(true);
+    try {
+      await subscribe({ priceId: priceData.id });
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
 
   console.log(JSON.stringify(product));
   console.log(JSON.stringify(prices));
@@ -25,10 +37,15 @@ const ProductCard = ({ product, prices }: { product: any; prices: any[] }) => {
         <p className="px-4 pt-8 text-3xl font-bold text-white">{priceText}</p>
         <div className="px-5 pt-10">
           <Button
-            onClick={() => subscribe({ priceId: priceData.id })}
+            onClick={handleSubscribe}
+            disabled={loading}
             className="focus:shadow-outline-blue w-full rounded-sm bg-[#42A5F5] px-4 py-2 text-white hover:bg-gray-700 focus:border-gray-700 focus:outline-none active:bg-gray-800"
           >
-            subscribe
+            {loading ? (
+              <CircularProgress size={24} color="inherit" />
+            ) : (
+              'subscribe'
+            )}
           </Button>
         </div>
       </div>
