c2fdc1ff0a2f9edbdc2be28f8b8528fa208913a0
Johannes Naylor <jonaylor89@gmail.com>
2023-08-08 22:21:09 +0200

    stuff

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material';
 import Image from 'next/image';
 
-const ProductCard = ({ product }: { product: any }) => {
+const ProductCard = ({ product, prices }: { product: any, prices: any[] }) => {
   /*
     // set product data
     container.querySelector('h2').innerText = product.name.toUpperCase();
@@ -39,24 +39,30 @@ const ProductCard = ({ product }: { product: any }) => {
     products.appendChild(container);
   });
 */
+  const priceData = prices[0];
+  const priceText = `${new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: priceData.currency,
+  }).format(((priceData.unit_amount as number) / 100).toFixed(2))} per ${
+    priceData.interval ?? 'once'
+  }`;
 
   console.log(JSON.stringify(product));
+  console.log(JSON.stringify(prices));
   return (
-    <div className="allign-center flex justify-center rounded-lg bg-white shadow">
-      <div>
-        <p className="text-black">{product?.name ?? 'UNKNOWN'}</p>
-        <p className="text-black">{product?.role ?? 'UNKNOWN'}</p>
-        <Image
-          src={product?.images[0]}
-          alt={product?.name}
-          width={200}
-          height={200}
-        />
-        <div>
-          <Button>Pricing</Button>
-        </div>
-      </div>
-    </div>
+    <>
+      <p className="text-white">
+        {product?.name.toUpperCase() ?? 'UNKNOWN'}
+      </p>
+      <p className="text-white">
+        {product?.role ?? 'UNKNOWN'}
+      </p>
+      <p className="text-white">
+        {priceText}
+      </p>
+      <Image src={product?.images[0]} alt={product?.name} width={200} height={200} />
+      <Button onClick={() => subscribe({ priceId: priceData.id })}>subscribe</Button>
+    </>
   );
 };
 
