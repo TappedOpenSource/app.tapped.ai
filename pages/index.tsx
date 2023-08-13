b5ef126adcfec75e3a1a758b8a3cb025758c3918
Johannes Naylor <jonaylor89@gmail.com>
2023-08-13 20:06:35 +0200

    application complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             Download the app
           </a>
         </div>
-        <Link href="/branding" className="hidden lg:block">already a member?</Link>
+        <Link href="/signup_complete" className="hidden lg:block">already a member?</Link>
       </div>
 
       <div className="flex flex-col place-items-center md:pt-24 md:pr-24 md:pl-24 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
@@ -48,7 +48,7 @@ export default function Home() {
           id="apply-here"
           className="px-4 py-2 mt-8 mb-4 text-lg font-bold rounded-lg hover:scale-105 transform transition-all duration-200 ease-in-out shadow-lg"
         >apply here</Link>
-        <Link href="/login" className="block lg:hidden text-black">already a member?</Link>
+        <Link href="/signup_complete" className="block lg:hidden text-black">already a member?</Link>
       </div>
     </main>
   );
--- pages/signup_complete.tsx
+++ pages/signup_complete.tsx
@@ -1,14 +1,20 @@
+import withSubscription from '@/domain/auth/withSubscription';
 import type { NextPage } from 'next';
 
 const SignupComplete: NextPage = () => {
   return (
     <>
-      <h1>Application Complete</h1>
-      <p>
-        thank you for your application. we will contact you shortly.
-      </p>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <h1 className="text-6xl text-center font-bold">Application Complete</h1>
+        <p className="text-center">
+        thank you for your application.
+        </p>
+        <p className="text-center">
+          we will contact you shortly.
+        </p>
+      </div>
     </>
   );
 };
 
-export default SignupComplete;
+export default withSubscription(SignupComplete);
