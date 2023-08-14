22a0e80a44b913e8cec53aa129f5f8a566829f0f
Johannes Naylor <jonaylor89@gmail.com>
2023-08-14 23:48:58 +0200

    artistname

--- components/application/name_field.tsx
+++ components/application/name_field.tsx
@@ -24,7 +24,7 @@ const NameField = ({ formData, updateFormData, onValidation }) => {
     <div className="page flex h-full flex-col items-center justify-center bg-white">
       <div className="flex w-full flex-col items-start px-6">
         <h1 className="mb-4 text-2xl font-bold text-[#42A5F5]">
-          What is your name/artist name?
+          What is your artist name?
         </h1>
         <div className="flex h-full w-full items-center justify-center">
           <input
