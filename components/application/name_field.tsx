e3f683f3e842d3cc050c46a5527c52d360d90e5c
Armaan Hossain <armaanhossain213@gmail.com>
2023-08-14 18:12:53 -0400

    fixing login

--- components/application/name_field.tsx
+++ components/application/name_field.tsx
@@ -23,8 +23,8 @@ const NameField = ({ formData, updateFormData, onValidation }) => {
   return (
     <div className="page flex h-full flex-col items-center justify-center bg-white">
       <div className="flex w-full flex-col items-start px-6">
-        <h1 className="mb-4 text-2xl font-bold text-[#42A5F5]">
-          What is your artist name?
+        <h1 className="mb-4 text-xl text-[#42A5F5]">
+          what is your artist name?
         </h1>
         <div className="flex h-full w-full items-center justify-center">
           <input
