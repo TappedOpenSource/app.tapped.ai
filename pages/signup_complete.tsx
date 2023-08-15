b6dc66ba0762bf63149763f4e665f05d632fd383
Johannes Naylor <jonaylor89@gmail.com>
2023-08-15 18:58:13 +0200

    add link

--- pages/signup_complete.tsx
+++ pages/signup_complete.tsx
@@ -7,6 +7,7 @@ import { Button } from '@mui/material';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import RiveComponent from '@rive-app/react-canvas';
+import Link from 'next/link';
 
 const SignupComplete: NextPage = () => {
   // const appleUrl = 'https://apps.apple.com/us/app/tapped-app/id1574937614';
@@ -38,7 +39,7 @@ const SignupComplete: NextPage = () => {
             </div>
             <h1 className="text-black text-2xl md:text-6xl text-center font-bold">reviewing your profile</h1>
             <p className="text-black text-center">
-              hang tight to see if you&apos;re accepted
+              hang tight to see if you&apos;re accepted.
             </p>
             <p className="text-black text-center">
               (don&apos;t close this window)
@@ -54,12 +55,15 @@ const SignupComplete: NextPage = () => {
       <div className="h-screen flex flex-col justify-center items-center">
         <h1 className="text-2xl md:text-6xl text-center font-bold">application complete</h1>
         <p className="text-center">
-          you have been approved.
+          you have been approved!
         </p>
         <div className="pb-12"></div>
-        <button className="bg-[#202020] px-4 py-2 mt-8 mb-4 text-lg font-bold rounded-lg hover:scale-105 transform transition-all duration-200 ease-in-out shadow-lg">
+        <Link
+          href="/pricing"
+          className="bg-[#202020] px-4 py-2 mt-8 mb-4 text-lg font-bold rounded-lg hover:scale-105 transform transition-all duration-200 ease-in-out shadow-lg"
+        >
           reserve your spot here
-        </button>
+        </Link>
         {/* <div className="pb-8"></div>
         <div className="flex justify-center items-center flex-col md:flex-row gap-4">
           <GooglePlayButton
