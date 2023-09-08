07cdda9f43254212715f0cda882aaada2d32420b
Armaan Hossain <armaanhossain213@gmail.com>
2023-09-07 21:57:03 -0400

    fixed app margins

--- components/SegmentedLine.tsx
+++ components/SegmentedLine.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SegmentedLine = ({ totalPages, currentIndex }) => {
   return (
-    <div className="flex justify-between absolute top-10 left-5 right-3">
+    <div className="flex justify-between absolute top-10 left-5 right-3 md:top-16 md:left-16 md:right-16">
       {Array.from({ length: totalPages }).map((_, index) => (
         <div
           key={index}
-          className={`w-8 rounded-full h-1 bg-[#8ac3f8] mr-1 ${currentIndex >= index ? 'bg-[#fff]' : ''}`}
+          className={`w-8 md:w-36 rounded-full h-1 md:h-2 bg-[#8ac3f8] mr-1 md:mr-3 ${currentIndex >= index ? 'bg-[#fff]' : ''}`}
         ></div>
       ))}
     </div>
@@ -14,5 +14,3 @@ const SegmentedLine = ({ totalPages, currentIndex }) => {
 };
 
 export default SegmentedLine;
-
-
