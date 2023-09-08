c967231ee9872d870470c3732d0e1dd1f5488aa4
Johannes Naylor <jonaylor89@gmail.com>
2023-09-07 23:49:26 -0400

    rm

--- components/landing/SupportJourney.tsx
+++ components/landing/SupportJourney.tsx
@@ -3,14 +3,14 @@ const SupportJourney = () => {
   return (
     <section id="support-journey">
       <div className="flex flex-col place-items-center justify-center">
-        <div className="bg-white/20 rounded-full">
+        {/* <div className="bg-white/20 rounded-full">
           <p
             className="text-center text-white py-4 px-8"
           >
             here to support your creative journey
           </p>
         </div>
-        <div className="h-8"></div>
+        <div className="h-8"></div> */}
         <h2 className="text-center text-4xl mx-4 md:mx-0 md:w-1/2">
         giving artists the tools they need to succeed in the music industry
         </h2>
