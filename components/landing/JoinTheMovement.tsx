038a0e45f136a75381ffa2f11ab0df3f8dd0dad1
Johannes Naylor <jonaylor89@gmail.com>
2023-09-12 16:41:01 -0400

    youtube video in website

--- components/landing/JoinTheMovement.tsx
+++ components/landing/JoinTheMovement.tsx
@@ -2,10 +2,16 @@
 const JoinTheMovement = () => {
   return (
     <section>
-      <div className="flex justify-center">
-        <div className="w-3/4">
-          <h1 className="text-center text-xl md:text-5xl">join the movement today and witness the power of art and innovation combined</h1>
-        </div>
+      <div className="flex flex-col justify-center items-center">
+        <iframe
+          className="mb-12 w-5/6 md:w-1/2 aspect-video"
+          width={560}
+          height={315}
+          src="https://www.youtube.com/embed/QxNjQVzzzFE?si=XF3MPIIHwkhhDPgl"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+        <h1 className="text-center text-xl md:text-5xl w-3/4">join the movement today and witness the power of art and innovation combined</h1>
       </div>
     </section>
   );
