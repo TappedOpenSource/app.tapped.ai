3a2cd824b0fe1baf48ae9fa0f79c91ecbab53314
Johannes Naylor <jonaylor89@gmail.com>
2023-10-01 03:31:40 +0200

    styling

--- pages/signup_complete.tsx
+++ pages/signup_complete.tsx
@@ -23,42 +23,43 @@ const SignupComplete: NextPage = () => {
     router.push('/');
   };
 
-  // if (loading) {
-  //   return (
-  //     <>
-  //       <div className='h-screen w-screen flex justify-center items-center bg-white pr-4 pl-4'>
-  //         <div>
-  //           <div className="flex flex-row justify-center">
-  //             <RiveComponent
-  //               src="/loading_logo.riv"
-  //               className="base-canvas-size h-48 w-48" />
-  //           </div>
-  //           <h1 className="text-black text-2xl md:text-6xl text-center font-bold">reviewing your profile</h1>
-  //           <p className="text-black text-center">
-  //             hang tight to see if you&apos;re accepted.
-  //           </p>
-  //           <p className="text-black text-center">
-  //             (don&apos;t close this window.)
-  //           </p>
-  //         </div>
-  //       </div>
-  //     </>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <>
+        <div className='h-screen w-screen flex justify-center items-center bg-white pr-4 pl-4'>
+          <div>
+            <div className="flex flex-row justify-center">
+              <RiveComponent
+                src="/loading_logo.riv"
+                className="base-canvas-size h-48 w-48" />
+            </div>
+            <h1 className="text-black text-2xl md:text-6xl text-center font-bold">reviewing your profile</h1>
+            <p className="text-black text-center">
+              hang tight to see if you&apos;re accepted.
+            </p>
+            <p className="text-black text-center">
+              (don&apos;t close this window.)
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
       <div className="h-screen flex flex-col justify-center items-center">
-        <h1 className="text-2xl md:text-6xl text-center font-bold">you&apos;ve been approved!</h1>
+        <h1 className="text-3xl md:text-6xl text-center font-bold">you&apos;ve been approved!</h1>
         <div className="py-1"></div>
-        <p className="text-center w-1/3">
+        <p className="text-center w-4/5 md:w-1/3">
           due to a high volume of applications, we are currently only accepting around 10 artists every couple weeks.
-          we will notify you via SMS and email if you&pos;s selected for the next batch.
+          we will notify you via SMS and email if you&apos;re selected for the next batch.
           <br />
           <br />
           <br />
           <br />
-          if you&apos;d like to guarantee your spot in our next batch of beta testers,
+          if you want to guarantee your spot in our next batch of beta testers
+          (<span className='font-bold text-red-500'>and get a discount</span>),
           you can pre-order the first month using the link below.
         </p>
         <div className="py-1"></div>
