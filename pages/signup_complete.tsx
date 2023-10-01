551f1d5b2ebc28474edbc061cb7a074f09992fc8
Johannes Naylor <jonaylor89@gmail.com>
2023-10-01 03:13:05 +0200

    Update sign up copy

--- pages/signup_complete.tsx
+++ pages/signup_complete.tsx
@@ -23,42 +23,50 @@ const SignupComplete: NextPage = () => {
     router.push('/');
   };
 
-  if (loading) {
-    return (
-      <>
-        <div className='h-screen w-screen flex justify-center items-center bg-white pr-4 pl-4'>
-          <div>
-            <div className="flex flex-row justify-center">
-              <RiveComponent
-                src="/loading_logo.riv"
-                className="base-canvas-size h-48 w-48" />
-            </div>
-            <h1 className="text-black text-2xl md:text-6xl text-center font-bold">reviewing your profile</h1>
-            <p className="text-black text-center">
-              hang tight to see if you&apos;re accepted.
-            </p>
-            <p className="text-black text-center">
-              (don&apos;t close this window.)
-            </p>
-          </div>
-        </div>
-      </>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <>
+  //       <div className='h-screen w-screen flex justify-center items-center bg-white pr-4 pl-4'>
+  //         <div>
+  //           <div className="flex flex-row justify-center">
+  //             <RiveComponent
+  //               src="/loading_logo.riv"
+  //               className="base-canvas-size h-48 w-48" />
+  //           </div>
+  //           <h1 className="text-black text-2xl md:text-6xl text-center font-bold">reviewing your profile</h1>
+  //           <p className="text-black text-center">
+  //             hang tight to see if you&apos;re accepted.
+  //           </p>
+  //           <p className="text-black text-center">
+  //             (don&apos;t close this window.)
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
       <div className="h-screen flex flex-col justify-center items-center">
-        <h1 className="text-2xl md:text-6xl text-center font-bold">application complete</h1>
-        <p className="text-center">
-          you have been approved!
+        <h1 className="text-2xl md:text-6xl text-center font-bold">you&apos;ve been approved!</h1>
+        <div className="py-1"></div>
+        <p className="text-center w-1/3">
+          due to a high volume of applications, we are currently only accepting around 10 artists every couple weeks.
+          we will notify you via SMS and email if you&pos;s selected for the next batch.
+          <br />
+          <br />
+          <br />
+          <br />
+          if you&apos;d like to guarantee your spot in our next batch of beta testers,
+          you can pre-order the first month using the link below.
         </p>
-        <div className="pb-12"></div>
+        <div className="py-1"></div>
         <Link
-          href="/pricing"
-          className="bg-[#202020] px-4 py-2 mt-8 mb-4 text-lg font-bold rounded-lg hover:scale-105 transform transition-all duration-200 ease-in-out shadow-lg"
+          href="https://buy.stripe.com/00g9CseCE2jubC0bIM"
+          className="bg-blue-500 px-4 py-2 mt-8 mb-4 text-lg font-bold rounded-lg hover:scale-105 transform transition-all duration-200 ease-in-out shadow-lg"
         >
-          reserve your spot here
+          pre-order here
         </Link>
         <div className="pb-16"></div>
         <Button onClick={signout} className="text-white">
