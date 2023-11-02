12fb54abf9cd4a335a1eaf0f2a4d7221b4e83d3d
Johannes Naylor <jonaylor89@gmail.com>
2023-11-02 13:49:46 -0500

    submit not login

--- components/application/signup_field.tsx
+++ components/application/signup_field.tsx
@@ -2,18 +2,18 @@
 import { useRouter } from 'next/router';
 import database from '@/data/database';
 // import { LoginResult } from '@/data/auth';
-import { loginWithGoogle } from '@/domain/usecases/login';
+// import { loginWithGoogle } from '@/domain/usecases/login';
 
 const SignUpField = ({ formData, updateFormData, onValidation }) => {
   const router = useRouter();
 
-  const handleLogin = async () => {
-    try {
-      loginWithGoogle();
-      // console.log({ userId: response.uid });
-    } catch (err) {
-      console.error(err);
-    }
+  const handleClick = async () => {
+    // try {
+    //   loginWithGoogle();
+    //   // console.log({ userId: response.uid });
+    // } catch (err) {
+    //   console.error(err);
+    // }
 
     await database.createNewApplicationResponse({
       userId: 'anonymous',
@@ -30,30 +30,15 @@ const SignUpField = ({ formData, updateFormData, onValidation }) => {
       <div className="text-center">
         <div>
           <p className="text-lg font-bold text-white mb-4">
-            sign up and we&apos;ll email you our decision
+            submit and we&apos;ll email you our decision
           </p>
         </div>
         <div className="flex items-center justify-center w-[60%] mx-auto">
           <button
-            onClick={handleLogin}
-            className="google_btn"
+            onClick={handleClick}
+            className='font-extrabold text-black bg-white rounded-full px-8 py-2'
           >
-            <svg
-              className="-ml-1 mr-2 h-4 w-4"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 488 512"
-            >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-              ></path>
-            </svg>
-        continue with google
+            submit
           </button>
         </div>
       </div>
