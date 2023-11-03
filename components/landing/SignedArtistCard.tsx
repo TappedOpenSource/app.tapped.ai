c8ffa82b0f9323dcb8643824143b0e4d98556599
Johannes Naylor <jonaylor89@gmail.com>
2023-11-03 14:39:49 -0500

    signed

--- components/landing/SignedArtistCard.tsx
+++ components/landing/SignedArtistCard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const SignedArtistCard = ({ name, photo, url, signed = false }: {
+const SignedArtistCard = ({ name, photo, url, signed }: {
   name: string;
   photo: string;
   url: string;
@@ -26,7 +26,7 @@ const SignedArtistCard = ({ name, photo, url, signed = false }: {
             style={{ objectFit: 'cover' }} />
           <div className="absolute bg-gradient-to-t from-black to-30% bottom-0 left-0 w-full h-full rounded-2xl"></div>
           <p className="absolute font-bold text-xl bottom-0 left-0 pb-2 pl-2">{name}</p>
-          {signed && (
+          {signed === true && (
             <div className="relative bg-transparent w-56 h-56 overflow-hidden">
               <div className="absolute left-0 top-0 h-16 w-16">
                 <div className="absolute left-[-34px] top-[32px] w-[170px] transform -rotate-45 bg-red-600 text-center text-white font-bold py-1">
