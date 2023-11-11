95dc462c8be2eb757a817a78f664b22f0e73f5e0
Johannes Naylor <jonaylor89@gmail.com>
2023-11-11 15:09:37 -0600

    subheaders

--- pages/booking/[username].tsx
+++ pages/booking/[username].tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import database from '@/data/database';
 import { UserModel } from '@/domain/models/user_model';
 
@@ -40,8 +41,30 @@ export default function Page() {
 
   return (
     <>
-      <h1>{user.artistName}</h1>
-      <p>{user.email}</p>
+      <div className='relative h-[256px] w-screen overflow-hidden'>
+        <Image
+          src={user.profilePicture}
+          alt={`${user.artistName} profile picture`}
+          objectFit='cover'
+          objectPosition='center'
+          fill
+        />
+      </div>
+      <div className='py-4 px-6'>
+        <div>
+          <h1 className='text-4xl font-extrabold'>{user.artistName}</h1>
+          <p className='text-sm text-gray-500'>{user.username}</p>
+        </div>
+        <div>
+          <h2 className='text-2xl font-bold'>Services</h2>
+        </div>
+        <div className='text-2xl font-bold'>
+          <h2>Reviews</h2>
+        </div>
+        <div className='text-2xl font-bold'>
+          <h2>Booking History</h2>
+        </div>
+      </div>
     </>
   );
 }
