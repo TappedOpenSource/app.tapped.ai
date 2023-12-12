9ecc5f21d96c70707c2d0bb4e347e2346c26e386
Johannes Naylor <jonaylor89@gmail.com>
2023-12-12 15:53:27 -0500

    blah

--- components/landing/Testimonials.tsx
+++ components/landing/Testimonials.tsx
@@ -60,7 +60,7 @@ export default function Testimonials() {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-center items-center px-12">
         {testimonials.map((testimonial, index) => (
-          <div key={index}>
+          <div key={index} className='h-full'>
             <Testimonial {...testimonial} />
           </div>
         ))}
