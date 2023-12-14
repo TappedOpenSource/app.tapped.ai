cf77b63118ce48005470655eca40a2f6dde3dced
avidshotz <131557485+avidshotz@users.noreply.github.com>
2023-12-14 16:57:27 -0500

    formatting complete for signup page, I'm a legend!

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -28,13 +28,13 @@ const Signup: NextPage = () => {
   };
 
   return (
-    <div className="grid h-screen grid-cols-1 gap-2 rounded-lg bg-[#FFF] p-16 shadow-lg">
+    <div className="flex flex-col justify-center items-center min-h-screen rounded-lg p-16">
       <div className="flex items-center justify-center pb-5">
         <Image
-          src="/images/tapped_logo.png"
+          src="/images/icon_1024.png"
           alt="Tapped_Logo"
-          width={330}
-          height={90}
+          width={180}
+          height={180}
         />
       </div>
 
@@ -59,36 +59,33 @@ const Signup: NextPage = () => {
           </div>
         </div>
         <div className="mb-6 md:flex md:items-center">
-          <div className="md:w-1/3">
+          <div className= 'md:w-1/3'>
             <label
               className="mb-1 block pr-4 text-xs font-bold text-gray-500 md:mb-0 md:text-right"
-              htmlFor="inline-password"
-            >
-              Password
-            </label>
-          </div>
+            htmlFor="inline-password"
+          >
+            Password
+          </label><p></p>
+         </div>
           <div className="md:w-2/3">
-            <input
-              className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-              id="inline-password"
-              type="password"
-              onChange={(e) => setData({ ...data, password: e.target.value })}
-              value={data.password || ''}
-            />
-          </div>
-          <div className="md:flex md:items-center">
-            <div className="md:w-1/3">
-              <Link href={`/login?returnUrl=${returnTo}`}>
-                <button className="tapped_signup_btn">Login</button>
-              </Link>
-            </div>
-            <div className="md:w-2/3">
-              <button className="tapped_btn w-full" type="submit">
-                Sign Up
-              </button>
-            </div>
-          </div>
+          <input
+            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+            id="inline-password"
+            type="password"
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+            value={data.password || ''}
+          />
         </div>
+        </div>
+        <div className="mb-6 flex md:items-center">
+          <button className="tapped_btn w-full bg-blue-700 px-4 py-2 rounded-full text-white font-bold" type="submit">
+            Sign Up
+          </button></div>
+        <div className="mb-6 flex items-center"><div >
+          <Link href={`/login?returnUrl=${returnTo}`}>
+            <button className="tapped_signup_btn">Login</button>
+          </Link>
+        </div></div>
       </form>
     </div>
   );
