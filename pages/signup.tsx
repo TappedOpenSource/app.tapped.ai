93d764cd3d8d8ff9bc3ab06e5ffa90f5e6fed4d6
avidshotz <131557485+avidshotz@users.noreply.github.com>
2023-12-19 18:12:08 -0500

    password confirm is invisible now

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -101,7 +101,7 @@ const Signup: NextPage = () => {
           </div>
           <div className="md:w-2/3">
             <input
-              style={{borderColor: inputColor }} type = 'text'
+              style={{borderColor: inputColor }} type = 'password'
               className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:bg-white focus:outline-none"
               id="confirm-password"
               // type="password"
