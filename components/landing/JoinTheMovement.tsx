770736d84d400baf9ae2ca1301da4984b31364c4
Johannes Naylor <jonaylor89@gmail.com>
2023-12-22 12:55:41 -0500

    add op

--- components/landing/JoinTheMovement.tsx
+++ components/landing/JoinTheMovement.tsx
@@ -12,15 +12,6 @@ const JoinTheMovement = () => {
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         ></iframe>
-        <div className="h-2" />
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href="https://forms.gle/F18XCFnAXmyML2Bn8"
-          className='mx-2 md:mx-0 border border-gray-400/50 rounded-xl px-8 md:px-24 py-5 bg-white/10 text-white text-center'
-        >
-          add an opportunity
-        </Link>
         <div className="h-4" />
         <h1 className="text-center text-xl md:text-5xl w-3/4">join the movement today and witness the power of art and innovation combined</h1>
       </div>
