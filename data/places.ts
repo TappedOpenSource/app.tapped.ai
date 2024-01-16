5f099a354f60b0f75fe73b3fc68a16d1e49c42a6
Johannes Naylor <jonaylor89@gmail.com>
2024-01-15 23:44:01 -0500

    new places thing

--- data/places.ts
+++ data/places.ts
@@ -1,33 +1,11 @@
-
-// web only - restricted
-const googlePlacesKey = 'AIzaSyCNxMOlRATil90cBmPAkqqSMtU26q5V46U';
+import { functions } from '@/utils/firebase';
+import { httpsCallable } from 'firebase/functions';
 
 export async function getPlaceById(placeId: string) {
-  const fields = [
-    'id',
-    'displayName',
-    'addressComponents',
-    'location',
-    'photoMetadatas',
-  ];
-  const url = `https://places.googleapis.com/v1/places/${
-    placeId
-  }?fields=${
-    fields.join(',')
-  }?key=${
-    googlePlacesKey
-  }`;
-  // https://places.googleapis.com/v1/places/ChIJs5ydyTiuEmsR0fRSlU0C7k0?fields=id,displayName&key=API_KEY
+  const callable = httpsCallable(functions, 'getPlaceById');
 
-  const headers = {
-    'Content-Type': 'application/json',
-    'X-Goog-Api-Key': googlePlacesKey,
-  };
-
-  fetch(url, {
-    headers: headers,
-  })
-    .then((response) => response.json())
-    .then((result) => console.log({ result }))
-    .catch((error) => console.error('Error:', error));
+  const results = await callable({
+    'placeId': placeId,
+  });
+  return results.data;
 }
