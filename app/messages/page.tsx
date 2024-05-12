c4e7e3b9be2546a079c0b13b80b3c705fd3cd446
Johannes Naylor <jonaylor89@gmail.com>
2024-05-12 02:42:05 -0400

    pls

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -9,12 +9,16 @@ import {
   ChannelHeader,
   ChannelList,
   Chat,
+  InfiniteScroll,
   MessageInput,
   MessageList,
   Thread,
   Window,
   useCreateChatClient,
 } from "stream-chat-react";
+import "stream-chat-react/dist/css/v2/index.css";
+import "stream-chat-react/dist/css/v2/index.layout.css";
+import "./layout.css";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY ?? "";
 
@@ -40,22 +44,26 @@ export default function Page() {
     fetchToken();
   }, [currentUser]);
 
-  if (currentUser === null) {
-    const encodedPathname = encodeURIComponent(pathname);
-    redirect(`/login?return_url=${encodedPathname}`);
-  }
+  // if (currentUser === null) {
+  //   const encodedPathname = encodeURIComponent(pathname);
+  //   redirect(`/login?return_url=${encodedPathname}`);
+  // }
+  if (currentUser === null) return <div>loading...</div>;
   if (!client) return <div>loading...</div>;
 
   const filters = { members: { $in: [currentUser.id] }, type: "messaging" };
   return (
-    <>
+    <div id="root">
       <Chat client={client}>
-        <ChannelList
-          sort={{ last_message_at: -1 }}
-          filters={filters}
-          // options={options}
-          showChannelSearch
-        />
+        <div className="channel-list-container">
+          <ChannelList
+            sort={{ last_message_at: -1 }}
+            filters={filters}
+            // options={options}
+            Paginator={InfiniteScroll}
+            showChannelSearch
+          />
+        </div>
         <Channel>
           <Window>
             <ChannelHeader />
@@ -65,6 +73,6 @@ export default function Page() {
           <Thread />
         </Channel>
       </Chat>
-    </>
+    </div>
   );
 }
