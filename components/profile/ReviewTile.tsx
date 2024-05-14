95ec436152166b3b59cbf21d3ecb7f000bd30016
Johannes Naylor <jonaylor89@gmail.com>
2024-05-14 11:18:02 -0400

    card

--- components/profile/ReviewTile.tsx
+++ components/profile/ReviewTile.tsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react";
-import UserTile from "../UserTile";
-import { UserModel } from "@/domain/types/user_model";
-import { Review } from "@/domain/types/review";
 import { getUserById } from "@/data/database";
+import { Review } from "@/domain/types/review";
+import { UserModel } from "@/domain/types/user_model";
+import { useEffect, useState } from "react";
+import UserTile from "../UserTile";
 
-export default function ReviewTile({ review }: {
-    review: Review;
-}) {
+export default function ReviewTile({ review }: { review: Review }) {
   const [reviewer, setReviewer] = useState<UserModel | null>(null);
 
   useEffect(() => {
@@ -15,9 +13,8 @@ export default function ReviewTile({ review }: {
         return;
       }
 
-      const reviewerId = review.type === "booker" ?
-        review.performerId :
-        review.bookerId;
+      const reviewerId =
+        review.type === "booker" ? review.performerId : review.bookerId;
 
       const reviewer = await getUserById(reviewerId);
       setReviewer(reviewer ?? null);
@@ -27,9 +24,9 @@ export default function ReviewTile({ review }: {
 
   return (
     <>
-      <div className='rounded-xl p-4 bg-gray-900'>
+      <div className="bg-card rounded-xl p-4">
         <UserTile user={reviewer} />
-        <div className='h-2' />
+        <div className="h-2" />
         <p>{review.overallReview}</p>
       </div>
     </>
