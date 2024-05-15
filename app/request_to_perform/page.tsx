b48d5c95f66809de83d7ec176e403b3039aef22a
Johannes Naylor <jonaylor89@gmail.com>
2024-05-15 16:26:33 -0400

    request to perform

--- app/request_to_perform/page.tsx
+++ app/request_to_perform/page.tsx
@@ -1,7 +1,14 @@
 
 import { Button } from "@/components/ui/button";
 
-export default function Page() {
+export default function Page({ searchParams }: {
+  searchParams: { [key: string]: string };
+}) {
+  const rawVenueIds = searchParams["venue_ids"];
+  const venueIds = rawVenueIds ? rawVenueIds.split(",") : [];
+
+  console.log({ venueIds });
+
   return (
     <>
       <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
