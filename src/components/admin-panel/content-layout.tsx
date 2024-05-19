4ea7508cdd9c0b63f388bef06018f16c9275414b
Johannes Naylor <jonaylor89@gmail.com>
2024-05-19 03:11:05 -0400

    no borders padding or margin

--- src/components/admin-panel/content-layout.tsx
+++ src/components/admin-panel/content-layout.tsx
@@ -1,15 +1,17 @@
 import { Navbar } from "@/components/admin-panel/navbar";
+import { cn } from "@/lib/utils";
 
 interface ContentLayoutProps {
   title: string;
+  noPadding?: boolean;
   children: React.ReactNode;
 }
 
-export function ContentLayout({ title, children }: ContentLayoutProps) {
+export function ContentLayout({ title, children, noPadding = false }: ContentLayoutProps) {
   return (
     <div>
       <Navbar title={title} />
-      <div className="container pt-8 pb-8 px-4 sm:px-8">{children}</div>
+      <div className={cn("container", noPadding ? "" : "pt-8 pb-8 px-4 sm:px-8")}>{children}</div>
     </div>
   );
 }
--- src/components/dashboard-map/map.tsx
+++ src/components/dashboard-map/map.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent } from "../ui/card";
 export default function DashboardMap() {
   return (
     <>
-      <Card className="rounded-lg border-none mt-6">
+      <Card className="rounded-lg border-none">
         <CardContent className="p-2">
           <div className="rounded-xl flex justify-center items-center">
             <VenueMap />
