48786691e959c91aec7f8b6d55492769ca17a798
Johannes Naylor <jonaylor89@gmail.com>
2024-05-23 20:10:10 -0400

    coerce

--- src/components/onboarding/OnboardingForm.tsx
+++ src/components/onboarding/OnboardingForm.tsx
@@ -17,11 +17,11 @@ const formSchema = z.object({
   username: z.string().min(3).max(20),
   profilePicture: z.custom<File>(),
   instagramHandle: z.string().min(3).max(20).optional(),
-  instagramFollowers: z.number().int().positive(),
+  instagramFollowers: z.coerce.number().int().positive(),
   twitterHandle: z.string().min(3).max(20).optional(),
-  twitterFollowers: z.number().int().positive(),
+  twitterFollowers: z.coerce.number().int().positive(),
   tiktokHandle: z.string().min(3).max(20).optional(),
-  tiktokFollowers: z.number().int().positive(),
+  tiktokFollowers: z.coerce.number().int().positive(),
   eula: z.boolean(),
 });
 
