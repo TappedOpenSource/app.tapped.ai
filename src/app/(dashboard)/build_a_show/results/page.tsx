f334cb18f66a4973688121bb5f25b9eeb3e86373
Johannes Naylor <jonaylor89@gmail.com>
2024-05-24 14:57:59 -0400

    results table

--- src/app/(dashboard)/build_a_show/results/page.tsx
+++ src/app/(dashboard)/build_a_show/results/page.tsx
@@ -1,14 +1,15 @@
-"use client";
-
 import { ContentLayout } from "@/components/admin-panel/content-layout";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
-import { Button } from "@/components/ui/button";
-import { columns } from "@/components/venue_search/columns";
-import { DataTable } from "@/components/venue_search/data_table";
-import { useSearch } from "@/context/search";
-import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import ResultsTable from "@/components/venue_search/results_table";
+
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Page({ searchParams }: {
     searchParams: { [key: string]: string };
@@ -18,32 +19,14 @@ export default function Page({ searchParams }: {
   const lat = searchParams["lat"] ?? "0";
   const lng = searchParams["lng"] ?? "0";
   const radius = searchParams["radius"] ?? "250";
-  const { useSearchData } = useSearch();
 
   console.log({ capacity, genres, lat, lng, radius });
 
   const performerGenres = genres.length > 0 ? genres.split(",") : [];
-
-  const { data } = useSearchData("", {
-    hitsPerPage: 30,
-    maxCapacity: parseInt(capacity),
-    venueGenres: performerGenres,
-    lat: parseFloat(lat),
-    lng: parseFloat(lng),
-    radius: parseInt(radius),
-  });
-  const [rowSelection, setRowSelection] = useState({});
-  const table = useReactTable({
-    data: data ?? [],
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    onRowSelectionChange: setRowSelection,
-    state: {
-      rowSelection,
-    },
-  });
-
-  const venueIds = table.getFilteredRowModel().rows.map((row) => row.original.id);
+  const intCapacity = parseInt(capacity);
+  const intLat = parseInt(lat);
+  const intLng = parseInt(lng);
+  const intRadius = parseInt(radius);
 
   return (
     <>
@@ -67,20 +50,13 @@ export default function Page({ searchParams }: {
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-        <div className="container mx-auto py-10 overflow-y-scroll">
-          <div className="flex justify-end py-3">
-            <Button
-              disabled={table.getFilteredSelectedRowModel().rows.length === 0}
-            >
-              <Link
-                href={`/build_a_show/request_to_perform?venue_ids=${venueIds.join(",")}`}
-              >
-            request to perform
-              </Link>
-            </Button>
-          </div>
-          <DataTable table={table} />
-        </div>
+        <ResultsTable
+          genres={performerGenres}
+          lat={intLat}
+          lng={intLng}
+          radius={intRadius}
+          capacity={intCapacity}
+        />
       </ContentLayout>
     </>
   );
---
+++ src/components/venue_search/results_table.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { columns } from "@/components/venue_search/columns";
+import { DataTable } from "@/components/venue_search/data_table";
+import { useSearch } from "@/context/search";
+import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function ResultsTable({
+  capacity,
+  genres,
+  lat,
+  lng,
+  radius,
+}: {
+    capacity: number;
+    genres: string[];
+    lat: number;
+    lng: number;
+    radius: number;
+}) {
+  const { useSearchData } = useSearch();
+  const { data } = useSearchData("", {
+    hitsPerPage: 30,
+    maxCapacity: capacity,
+    venueGenres: genres,
+    lat: lat,
+    lng: lng,
+    radius: radius,
+  });
+  const [rowSelection, setRowSelection] = useState({});
+  const table = useReactTable({
+    data: data ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      rowSelection,
+    },
+  });
+
+  const venueIds = table.getFilteredRowModel().rows.map((row) => row.original.id);
+
+  return (
+    <>
+      <div className="container mx-auto py-10 overflow-y-scroll">
+        <div className="flex justify-end py-3">
+          <Button
+            disabled={table.getFilteredSelectedRowModel().rows.length === 0}
+          >
+            <Link
+              href={`/build_a_show/request_to_perform?venue_ids=${venueIds.join(",")}`}
+            >
+            request to perform
+            </Link>
+          </Button>
+        </div>
+        <DataTable table={table} />
+      </div>
+    </>
+  );
+}
