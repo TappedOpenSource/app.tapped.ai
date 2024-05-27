4ecc898a59bd6beaf04ccd5a19c1ad7326df8a6d
Johannes Naylor <jonaylor89@gmail.com>
2024-05-27 11:09:58 -0400

    no snap

--- src/components/profile/BookingHistoryPreview.tsx
+++ src/components/profile/BookingHistoryPreview.tsx
@@ -15,14 +15,10 @@ export default function BookingHistoryPreview({ user, bookings }: {
   return (
     <>
       <div className='flex justify-start items-center'>
-        <div className="flex flex-row items-start justify-start overflow-x-auto space-x-5 snap-x">
+        <div className="flex flex-row items-start justify-start overflow-x-auto space-x-5">
           {bookings.map((booking, index) => (
             <div key={index}>
-              <div
-                className='snap-start'
-              >
-                <BookingCard booking={booking} user={user} />
-              </div>
+              <BookingCard booking={booking} user={user} />
             </div>
           ))}
         </div>
