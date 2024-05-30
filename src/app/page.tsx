600b88033460035692530df316e9a06589cc4a19
Johannes Naylor <jonaylor89@gmail.com>
2024-05-30 16:51:55 -0400

    let's get it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,13 +40,22 @@ export default function Page() {
           >
             <SearchBar />
           </Suspense>
-          <Link
-            href="/map"
-          >
-            <Button variant={"link"}>
+          <div className="flex justify-center gap-4 mt-4">
+            <Link
+              href="/map"
+            >
+              <Button variant={"secondary"}>
               view the map <span className="ml-2"><Globe2 className="h-4 w-4" /></span>
-            </Button>
-          </Link>
+              </Button>
+            </Link>
+            <Link
+              href="/premium"
+            >
+              <Button variant={"ghost"}>
+                try premium
+              </Button>
+            </Link>
+          </div>
         </div>
         <FeaturedPerformers />
         <div className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3">
