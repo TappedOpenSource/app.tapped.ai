2dc828b4a42c45dad6b4fd4483fe8f76c8f1f8c1
Johannes Naylor <jonaylor89@gmail.com>
2024-07-04 19:38:18 -0400

    yes?

--- src/components/opportunity/ApplyButton.tsx
+++ src/components/opportunity/ApplyButton.tsx
@@ -13,9 +13,7 @@ import { useToast } from "../ui/use-toast";
 import { guardedApplyForOpportunity } from "@/domain/usecases/opportunities";
 import { usePurchases } from "@/context/purchases";
 
-export default function ApplyButton({ op }: {
-    op: Opportunity;
-}) {
+export default function ApplyButton({ op }: { op: Opportunity }) {
   const { state: authState } = useAuth();
   const { authUser } = authState;
   const { state: subscribed } = usePurchases();
@@ -46,34 +44,11 @@ export default function ApplyButton({ op }: {
     );
   }
 
-  if (!authUser) {
-    return (
-      <div>
-        <Link href={`/signup?return_url=${pathname}`}>
-          <Button className="bg-blue-500">
-            apply to perform
-          </Button>
-        </Link>
-      </div>
-    );
-  }
-
-
-  if (authUser.uid) {
-    <div>
-      <Link href={`/opportunity/${op.id}/applicants`}>
-        <Button aria-label="add">
-            see who&apos;s applied
-        </Button>
-      </Link>
-    </div>;
-  }
-
   if (isApplied) {
     return (
       <div>
         <Button disabled>
-          <Check className="h-4 w-4 mr-2" />
+          <Check className="mr-2 h-4 w-4" />
           <span>you&apos;re applied!</span>
         </Button>
       </div>
@@ -82,38 +57,14 @@ export default function ApplyButton({ op }: {
 
   return (
     <div>
-      <Button
-        className="bg-blue-500"
-        onClick={async () => {
-          setLoading(true);
-          try {
-            await guardedApplyForOpportunity({
-              opportunityId: op.id,
-              userId: authUser.uid,
-              isPremium: subscribed ?? false,
-            });
-            setIsApplied(true);
-            toast({
-              title: "application submitted!",
-              description: "this is a huge step in your career. the booker will be in touch if you're selected",
-            });
-          } catch (e) {
-            console.error("error applying for opportunity", e);
-            toast({
-              title: "you've run out of opportunities to apply for",
-              description: "join tapped premium to get unlimited gig opportunities",
-            });
-
-            router.push(
-              `/premium?return_url=/opportunities/${op.id}`
-            );
-          } finally {
-            setLoading(false);
-          }
+      <Link
+        href={{
+          pathname: "/opportunities/[opportunityId]/apply",
+          query: { opportunityId: op.id },
         }}
       >
-            apply to perform
-      </Button>
+        <Button className="bg-blue-500">apply to perform</Button>
+      </Link>
     </div>
   );
 }
