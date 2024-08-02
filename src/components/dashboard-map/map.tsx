73329ac6f8f8580e2867d430a37e37684b26c8eb
Johannes Naylor <jonaylor89@gmail.com>
2024-08-02 14:47:55 +0200

    view map button

--- src/components/dashboard-map/map.tsx
+++ src/components/dashboard-map/map.tsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion";
 // import dynamic from "next/dynamic";
 
 import { World } from "@/components/ui/globe";
+import { Button } from "../ui/button";
+import Link from "next/link";
 // const World = dynamic(() => import("../ui/globe").then((m) => m.World), {
 //   ssr: false,
 // });
@@ -397,32 +399,41 @@ export default function DashboardMap() {
 
   return (
     <>
-      <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
-        <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
-          <motion.div
-            initial={{
-              opacity: 0,
-              y: 20,
-            }}
-            animate={{
-              opacity: 1,
-              y: 0,
-            }}
-            transition={{
-              duration: 1,
-            }}
-            className="div"
+      <div className="relative">
+        <div className="absolute flex justify-center items-center h-full w-full z-10">
+          <Link
+            href="/map"
           >
-            <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
+            <Button variant={"secondary"}>view map</Button>
+          </Link>
+        </div>
+        <div className="z-0 flex flex-row items-center justify-center py-20 h-screen md:h-auto dark:bg-black bg-white relative w-full">
+          <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
+            <motion.div
+              initial={{
+                opacity: 0,
+                y: 20,
+              }}
+              animate={{
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{
+                duration: 1,
+              }}
+              className="div"
+            >
+              <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
               create a world tour from your iPhone
-            </h2>
-            <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
+              </h2>
+              <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
               decide your next show using facts and analytics to make every show a success
-            </p>
-          </motion.div>
-          <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-          <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
-            <World data={sampleArcs} globeConfig={globeConfig} />;
+              </p>
+            </motion.div>
+            <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
+            <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
+              <World data={sampleArcs} globeConfig={globeConfig} />;
+            </div>
           </div>
         </div>
       </div>
