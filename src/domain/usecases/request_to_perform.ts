7ff43d79fe7942d729504899f066937db4399933
Johannes Naylor <jonaylor89@gmail.com>
2024-08-05 15:05:15 +0200

    new request to perform

--- src/domain/usecases/request_to_perform.ts
+++ src/domain/usecases/request_to_perform.ts
@@ -1,30 +1,13 @@
-import { contactVenue } from "@/data/database";
 import type { UserModel } from "@/domain/types/user_model";
 import { functions } from "@/utils/firebase";
 import { httpsCallable } from "firebase/functions";
 
 export async function requestToPerform(currentUser: UserModel, venues: UserModel[], note: string) {
-  const callable = httpsCallable(functions, "sendEmailOnVenueContacting");
-  await Promise.all(
-    [
-      callable({
-        "userId": currentUser.id,
-      }),
-      ...venues.map(async (venue) => {
-        const bookingEmail = venue.venueInfo?.bookingEmail;
-
-        if (!bookingEmail) {
-          return;
-        }
-
-        return contactVenue({
-          currentUser,
-          venue,
-          note,
-          bookingEmail,
-          // collaborators: _collaborators,
-        });
-      }),
-    ],
-  );
+  const callable = httpsCallable(functions, "genericContactVenues");
+  await callable({
+    "userId": currentUser.id,
+    "venueIds": venues.map((v) => v.id),
+    "note": note,
+    "collaborators": [],
+  });
 }
