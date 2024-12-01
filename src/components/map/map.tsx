5a5eb3499e583ff081e404cf8760747bcb6acbe0
Johannes Naylor <jonaylor89@gmail.com>
2024-12-01 00:29:02 -0500

    refactor bottom nav bar

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -31,37 +31,7 @@ import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { cn } from "@/lib/utils";
 import LocationSideSheet from "./LocationSideSheet";
 import { trackEvent } from "@/utils/tracking";
-import {
-  DropdownMenu,
-  DropdownMenuItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuGroup,
-} from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { Card } from "../ui/card";
-import NumberFlow from "@number-flow/react";
-import Link from "next/link";
-import {
-  Bug,
-  Download,
-  Gem,
-  Home,
-  LogOut,
-  MapIcon,
-  MessageCircle,
-  Search,
-  Settings,
-  Theater,
-  User,
-  User2,
-} from "lucide-react";
-import { useSearchToggle } from "@/context/use-search-toggle";
-import { useStore } from "@/context/use-store";
-import { logout } from "@/data/auth";
+import BottomNavBar from "./BottomNavBar";
 
 const env = process.env.NODE_ENV || "development";
 const defaultMapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
@@ -102,7 +72,6 @@ function _VenueMap({
   zoom: number;
 }) {
   const [bounds, setBounds] = useState<null | BoundingBox>(null);
-  const searchBar = useStore(useSearchToggle, (state) => state);
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
   const { useVenueData } = useSearch();
   const debouncedBounds = useDebounce<null | BoundingBox>(bounds, LIMIT);
@@ -112,9 +81,6 @@ function _VenueMap({
   } = useAuth();
   const { state: subscribed } = usePurchases();
   const { resolvedTheme } = useTheme();
-  const isLoggedIn = authUser !== null;
-  const displayName = currentUser?.artistName ?? currentUser?.username ?? "use";
-  const email = currentUser?.email ?? authUser?.email ?? "user@tapped.ai";
 
   // redirect to signup if user is not logged in
   useEffect(() => {
@@ -128,7 +94,7 @@ function _VenueMap({
       }
 
       router.push("/signup?return_url=/map");
-    }, 10_000);
+    }, 15_000);
 
     return () => {
       clearTimeout(timeout);
@@ -245,14 +211,6 @@ function _VenueMap({
     return newMarkers;
   }, [data, currentUser, subscribed, router, isFetching]);
 
-  const markersCount = useMemo(() => {
-    if (isFetching) {
-      return cachedMarkers.current.length;
-    }
-
-    return markers.length;
-  }, [isFetching, markers]);
-
   const mapTheme =
     resolvedTheme === "light" ? mapboxLightStyle : mapboxDarkStyle;
   return (
@@ -266,162 +224,13 @@ function _VenueMap({
         onOpenChange={() => setSidebarIsOpen(!sidebarIsOpen)}
       />
       <div className="fixed inset-0 z-0 flex h-full w-full flex-col overflow-hidden">
-        <div className="absolute bottom-0 z-40 flex w-full justify-center">
-          <Card className="m-3 flex gap-3 p-1">
-            <Button
-              variant="outline"
-              className={cn("flex gap-1")}
-              onClick={() => {
-                trackEvent("menu_click");
-                setSidebarIsOpen(!sidebarIsOpen);
-              }}
-            >
-              <span>
-                {markersCount === LIMIT && "+"}
-                <NumberFlow value={markersCount} />
-              </span>
-              venues
-            </Button>
-            <Link href="/venue_outreach" className="hidden lg:block">
-              <Button variant="secondary">apply to perform</Button>
-            </Link>
-
-            <div className="flex gap-1">
-              <Link href="/venue_outreach" className="lg:hidden">
-                <Button size="icon" variant="secondary">
-                  <Theater className="size-3" />
-                </Button>
-              </Link>
-              <Link href="/messages">
-                <Button size="icon" variant="secondary">
-                  <MessageCircle className="size-3" />
-                </Button>
-              </Link>
-              <Button
-                size="icon"
-                variant="secondary"
-                onClick={() => searchBar?.setIsOpen()}
-              >
-                <Search className="size-3" />
-              </Button>
-              <Link href="/download" className="lg:hidden">
-                <Button size="icon" variant="secondary">
-                  <Download className="size-3" />
-                </Button>
-              </Link>
-              {isLoggedIn ? (
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button size="icon" variant="ghost">
-                      <Avatar className="h-8 w-8">
-                        <AvatarImage
-                          src={currentUser?.profilePicture ?? undefined}
-                          alt="Avatar"
-                        />
-                        <AvatarFallback className="bg-transparent">
-                          {currentUser?.username.slice(0, 2) ?? "JD"}
-                        </AvatarFallback>
-                      </Avatar>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-56" align="end" forceMount>
-                    <DropdownMenuLabel className="font-normal">
-                      <div className="flex flex-col space-y-1">
-                        <p className="text-sm font-medium leading-none">
-                          {displayName}
-                        </p>
-                        <p className="text-muted-foreground text-xs leading-none">
-                          {email}
-                        </p>
-                      </div>
-                    </DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem
-                        className="hover:cursor-pointer"
-                        asChild
-                      >
-                        <Link href="/" className="flex items-center">
-                          <Home className="text-muted-foreground mr-3 h-4 w-4" />
-                          dashboard
-                        </Link>
-                      </DropdownMenuItem>
-                      {currentUser?.username !== undefined && (
-                        <>
-                          <DropdownMenuItem
-                            className="hover:cursor-pointer"
-                            asChild
-                          >
-                            <Link
-                              href={`/u/${currentUser?.username ?? ""}`}
-                              className="flex items-center"
-                            >
-                              <User className="text-muted-foreground mr-3 h-4 w-4" />
-                              public profile
-                            </Link>
-                          </DropdownMenuItem>
-                        </>
-                      )}
-                      <DropdownMenuItem
-                        className="hover:cursor-pointer"
-                        asChild
-                      >
-                        <Link href="/billing" className="flex items-center">
-                          <Gem className="text-muted-foreground mr-3 h-4 w-4" />
-                          billing
-                        </Link>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        className="hover:cursor-pointer"
-                        asChild
-                      >
-                        <Link
-                          href="https://tapped.canny.io/ideas-bugs"
-                          className="flex items-center"
-                        >
-                          <Bug className="text-muted-foreground mr-3 h-4 w-4" />
-                          report bugs
-                        </Link>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        className="hover:cursor-pointer"
-                        asChild
-                      >
-                        <Link href="/settings" className="flex items-center">
-                          <Settings className="text-muted-foreground mr-3 h-4 w-4" />
-                          settings
-                        </Link>
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem className="hover:cursor-pointer" asChild>
-                      <Link href="/download" className="flex items-center">
-                        <Download className="text-muted-foreground mr-3 h-4 w-4" />
-                        get the app
-                      </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      className="hover:cursor-pointer"
-                      onClick={() => {
-                        logout();
-                        router.push("/");
-                      }}
-                    >
-                      <LogOut className="text-muted-foreground mr-3 h-4 w-4" />
-                      sign out
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              ) : (
-                <Link href={`/signup?return_url=${encodeURIComponent("/map")}`}>
-                  <Button size="icon">
-                    <User2 className="size-3" />
-                  </Button>
-                </Link>
-              )}
-            </div>
-          </Card>
-        </div>
+        <BottomNavBar
+          queryLimit={LIMIT}
+          markers={markers}
+          cachedMarkers={cachedMarkers}
+          isFetching={isFetching}
+          onVenueCountClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+        />
         <div className="relative flex-1">
           <MapBoxGL
             style={{ height: "100vh !important", width: "100% !important" }}
