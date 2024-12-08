b7120116e1e5ee5da815d47b63282845eb719288
Johannes Naylor <jonaylor89@gmail.com>
2024-12-07 22:33:52 -0500

    unauth header

--- src/app/(home)/venue_outreach/page.tsx
+++ src/app/(home)/venue_outreach/page.tsx
@@ -1,34 +1,11 @@
+import UnauthHeader from "@/components/unauth_header";
 import VenueSearchForm from "@/components/venue_search/VenueSearchForm";
-import { ContentLayout } from "@/components/admin-panel/content-layout";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import Link from "next/link";
 
 export default function Page() {
   return (
     <>
-      <ContentLayout title="venue outreach">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link href="/">home</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>venue outreach</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-        <VenueSearchForm />
-      </ContentLayout>
+      <UnauthHeader />
+      <VenueSearchForm />
     </>
   );
 }
